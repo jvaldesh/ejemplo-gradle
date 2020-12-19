56a1323add8e6f1d7b4696bcce52044c1ae44a0b
Julio Valdés <jvaldesh@gmail.com>
2020-12-19 19:23:14 -0300

    Fix maven script

--- maven.groovy
+++ maven.groovy
@@ -1,34 +1,24 @@
 def call() {
     stage('compile') {
-        steps {
-            sh "./mvnw clean compile -e"
-        }
+        sh "./mvnw clean compile -e"
     }
 
     stage('test') {
-        steps {
-            sh "./mvnw clean test -e"
-        }
+        sh "./mvnw clean test -e"
     }
 
     stage('jar') {
-        steps {
-            sh "./mvnw clean package -e"
-        }
+        sh "./mvnw clean package -e"
     }
 
     stage('SonarQube') {
-        steps {
-            withSonarQubeEnv(installationName: 'sonarqube') {
-                sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-            }
+        withSonarQubeEnv(installationName: 'sonarqube') {
+            sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
         }
     }
 
     stage('uploadNexus') {
-        steps {
-            nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/home/julio/repos/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
-        }
+        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-repo', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: '/home/julio/repos/ejemplo-maven/build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
     }
 }
 
